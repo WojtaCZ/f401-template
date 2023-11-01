62c22adc61592e428083888d3312e0318d5bbe62
Vojtěch Vosáhlo <vosahlov@gmail.com>
2023-11-01 21:29:52 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "main.hpp"
+#include "stm32f401xe.h"
+#include "core_cm4.h"
+#include "cmsis_compiler.h"
+
+extern "C" void SystemInit(void){
+	//Initialize system here	
+}
+
+extern "C" int main(void){
+	//Do something here
+}
+
+extern "C" void HardFault_Handler(void){
+	//Ooops, hard fault!
+}
+
+
+
+
+
