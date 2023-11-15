092ce31135a4151a0d9fd929a8c78bbfdb65764c
Vojtěch Vosáhlo <vosahlov@gmail.com>
2023-11-15 10:32:21 +0100

    Added task file

--- build/CMakeFiles/3.27.7/CMakeSystem.cmake
+++ build/CMakeFiles/3.27.7/CMakeSystem.cmake
@@ -3,13 +3,13 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.2.16-2-MANJARO")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/home/vosahlov/Documents/programovani/MAM/mam_template/cmake/toolchain-arm-none-eabi.cmake")
 
-set(CMAKE_SYSTEM "Generic")
-set(CMAKE_SYSTEM_NAME "Generic")
-set(CMAKE_SYSTEM_VERSION "")
-set(CMAKE_SYSTEM_PROCESSOR "ARM")
 
-set(CMAKE_CROSSCOMPILING "TRUE")
+set(CMAKE_SYSTEM "Linux-6.2.16-2-MANJARO")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "6.2.16-2-MANJARO")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "FALSE")
 
 set(CMAKE_SYSTEM_LOADED 1)
